9bbaf7c4cb50677d3a577fe08283da9e267c9064
sdtsui <danielt213@gmail.com>
2015-07-02 10:39:17 -0700

    added twitter

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ app.use(express.static(__dirname));
 app.listen(port);
 
 console.log('Listening on PORT ' + port + '....');
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: 'bnzNvrZpwC2wkQAqauvCPIfBt',
+  consumer_secret: 'xqs953PfR6Zqjj2Q786fpMeQ8Z6NkoFCXK137xVTgtFCnwTVuU',
+  access_token_key: '2719284156-FCzrVGXcRUZQKvO0j05BQiNxTeWNx9Tb1areGcP',
+  access_token_secret: 'zDpOuxLUTief5qC6hryIYFPIcW2URHOrIVAJ9N0Ve9rYb'
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+  console.log('req attempted:');
+});
